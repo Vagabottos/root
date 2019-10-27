db2b9348f5f13b79ea1d84d7f18a38fc6b334e0e
seiyria <kyle@seiyria.com>
2019-10-27 10:59:22 -0500

    add rule comparison, closes #11

--- scripts/compare-rules.js
+++ scripts/compare-rules.js
@@ -7,17 +7,19 @@ const file = fs.readFileSync('src/assets/rules.yml', 'UTF-8');
 const rules = yaml.safeLoad(file);
 
 const x = xray({
+  /*
   filters: {
     trim: function(value) {
       return typeof value === 'string' ? value.trim() : value
     }
   }
+  */
 });
 
 const getLivingRules = async () => {
   let res = await x('http://root.livingrules.io', '#body-container ol');
 
-  const replacers = ["’", "'"];
+  const replacers = ["’"];
   replacers.forEach(rep => res = res.split(rep).join("'"));
 
   const replacersD = ['"', '“', '”', '”', '“'];
@@ -26,10 +28,29 @@ const getLivingRules = async () => {
   const ignoreStrings = [
     'These final two sections refer to factions a	vailable in The Riverfolk Expansion.',
     'This phase has two steps in the following order.',
-    'Your Evening has four steps, as follows.'
+    'Your Evening has four steps, as follows.',
+    `Catering to those creatures who have been discarded by the other factions, the Lizard Cult seeks to overwhelm its foes through sheer force of will. The Cult relies on word of mouth and beak to spread its gospel, and new enclaves can spring up anywhere on the map.`,
+    `Where the Cult rule clearings, they can build gardens, which will further radicalize the animals that live there. While other factions spend cards to achieve their aims, the Cult acts chiefly by revealing cards and gradually drafting the ideal set of followers.`,
+    `Unless used for scoring, these cards are not spent and are returned to the Cult's hand during Evening. However, this gentler approach makes movement and combat operations difficult, so these actions can only be undertaken by the most radicalized members of the Cult.`,
+    'Step 2: Roll Dice and Add Extra Hits.'
   ];
 
   ignoreStrings.forEach(str => res = res.replace(str, ''));
+
+  const strictReplacers = [
+    { repl: '(4.2.2.I)', replVal: '(4.3.2.1)' },
+    { repl: '(4.3.2.III)', replVal: '(4.3.2.3)' },
+    { repl: '(9.2.9.IIIb)', replVal: '(9.2.9.3.2)' },
+    { repl: '(9.2.9.IIb)', replVal: '(9.2.9.2.2)' },
+    { repl: '(9.2.9.IIc)', replVal: '(9.2.9.2.3)' },
+    { repl: '(9.2.9.III)', replVal: '(9.2.9.3)' },
+  ];
+
+  strictReplacers.forEach(({ repl, replVal }) => {
+    res = res.replace(repl, replVal);
+  });
+
+  // res = res.replace(/\s\s+/g, ' ');
   /*
   lol @ thinking these are organized enough to do this-
 
@@ -131,6 +152,8 @@ const cleanText = (text) => {
   text = text.replace(/\s\s+/g, ' ');
   text = text.split(' . ').join('');
 
+  if(text.length < 100) text = '';
+
   return text;
 };
 
@@ -139,10 +162,12 @@ const init = async () => {
     const ruleBlob = await getLivingRules();
     const remainingText = unparseText(ruleBlob, rules);
     const clean = cleanText(remainingText);
-    console.log(clean);
 
-    console.log(clean.indexOf(`Catering to those creatures who have been discarded by the other factions, the Lizard Cult seeks to overwhelm its foes through sheer force of will. The Cult relies on word of mouth and beak to spread its gospel, and new enclaves can spring up anywhere on the map. Where the Cult rule clearings, they can build gardens, which will further radicalize the animals that live there. While other factions spend cards to achieve their aims, the Cult acts chiefly by revealing cards and gradually drafting the ideal set of followers. Unless used for scoring, these cards are not spent and are returned to the Cult's hand during`))
-    // console.log(rules);
+    if(clean) {
+      console.error('Some rules were updated.');
+      console.error(clean);
+      process.exit(1);
+    }
   } catch(e) {
     console.error('INVALID RULE ERROR', e);
   }
