7dbe116c1f9498a6895e11cb74acda7ed646f5f7
Kyle J. Kemp <kyle@seiyria.com>
2020-05-28 21:13:19 -0500

    one day I'll learn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -29,7 +29,6 @@ export class HomePage implements OnInit, AfterContentInit, OnDestroy {
 
   ngOnInit() {
     this.nav$ = this.rulesService.navigate$.subscribe(id => this.scrollToEl(id, 'start'));
-    this.openMaps();
   }
 
   ngAfterContentInit() {
