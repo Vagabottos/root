6b5896bc5d12ee4be47352f2f17c29f2ab7ee15b
Evgeniy Oktyabrskiy <evgeniy.oktyabrskiy@ingrammicro.com>
2023-08-22 01:21:25 +0300

    ru-RU localization

--- src/app/rules.service.ts
+++ src/app/rules.service.ts
@@ -9,11 +9,13 @@ import { Subject } from 'rxjs';
 import * as enUSRules from '../assets/i18n/rules/en-US.json';
 import * as esESRules from '../assets/i18n/rules/es-ES.json';
 import * as deDERules from '../assets/i18n/rules/de-DE.json';
+import * as ruRURules from '../assets/i18n/rules/ru-RU.json';
 
 const rules = {
   'en-US': (enUSRules as any).default || enUSRules,
   'es-ES': (esESRules as any).default || esESRules,
   'de-DE': (deDERules as any).default || deDERules,
+  'ru-RU': (ruRURules as any).default || ruRURules,
 };
 
 @Injectable({
