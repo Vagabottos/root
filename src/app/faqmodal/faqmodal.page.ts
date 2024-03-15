5fcefe0f4e3a452bbc60f70636a7eb295d32c47d
Lukasz Bytnar <lukasz.bytnar@tfgm.com>
2024-03-15 10:00:05 +0000

    Add Polish menu items

--- src/app/faqmodal/faqmodal.page.ts
+++ src/app/faqmodal/faqmodal.page.ts
@@ -6,12 +6,14 @@ import * as enUSFAQ from '../../assets/i18n/faq/en-US.json';
 import * as esESFAQ from '../../assets/i18n/faq/es-ES.json';
 import * as deDEFAQ from '../../assets/i18n/faq/de-DE.json';
 import * as ruRUFAQ from '../../assets/i18n/faq/ru-RU.json';
+import * as plPLFAQ from '../../assets/i18n/faq/pl-PL.json';
 
 const faq = {
   'en-US': (enUSFAQ as any).default || enUSFAQ,
   'es-ES': (esESFAQ as any).default || esESFAQ,
   'de-DE': (deDEFAQ as any).default || deDEFAQ,
   'ru-RU': (ruRUFAQ as any).default || ruRUFAQ,
+  'pl-PL': (plPLFAQ as any).default || plPLFAQ,
 };
 
 @Component({
